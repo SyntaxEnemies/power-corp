7cc903c72447dcb4aa51d9fcc59c59056958563e
Rahul Sharma <rsharma842756@gmail.com>
2022-10-30 11:21:07 +0530

    ignoring Zone.Identifier files and removed exception handling from DBcm

--- DBcm.py
+++ DBcm.py
@@ -1,43 +1,24 @@
 import mariadb
 
 
-class ConnectionError(Exception):
-	pass
-
-class CredentialsError(Exception):
-	pass
-
-class SQLError(Exception):
-	pass
-
-
 class UseDatabase():
-	def __init__(self, config: dict) -> None:
-		self.configuration = config
-		# dunder init takes care of all object creation argument(s).
+    def __init__(self, config: dict) -> None:
+        self.configuration = config
+        # dunder init takes care of all object creation argument(s).
 
 
-	def __enter__(self) -> 'MariaDB cursor':
-		try:
-			self.conn = mariadb.connect(**self.configuration)
-			self.cursor = self.conn.cursor()
-			return self.cursor
-		# The 'conn' and 'cursor' are prefixed with 'self' 
-		# so that they survive for use in 'dunder exit' 
-
-		except mariadb.InterfaceError as err:
-			raise ConnectionError(err)
-
-		except mariadb.ProgrammingError as err:
-			raise CredentialsError(err)
+    def __enter__(self) -> 'MariaDB cursor':
+        self.conn = mariadb.connect(**self.configuration)
+        self.cursor = self.conn.cursor()
+        return self.cursor
+        # The 'conn' and 'cursor' are prefixed with 'self' 
+        # so that they survive for use in 'dunder exit' 
 
 
-	def __exit__(self, exc_type, exc_value, exc_trace) -> None:
-		self.conn.commit()
-		self.cursor.close()
-		self.conn.close()
+    def __exit__(self, exc_type, exc_value, exc_trace) -> None:
+        self.conn.commit()
+        self.cursor.close()
+        self.conn.close()
 
-		if exc_type is mariadb.ProgrammingError:
-			raise SQLError(exc_value)
-		elif exc_type:
-			raise exc_type(exc_value)
+        if exc_type:
+            raise exc_type(exc_value)
