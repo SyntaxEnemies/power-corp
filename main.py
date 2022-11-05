aa5933a20f264542678b74751398b77bb90ab9d5
Rahul Sharma <rsharma842756@gmail.com>
2022-11-05 21:31:05 +0530

    converting to single quotes and removing unnecessary templates

--- main.py
+++ main.py
@@ -8,15 +8,15 @@ app.config.from_envvar('CONFIG_FILE')
 # print(app.config)
 
 
-@app.route("/", methods=["GET"])
-@app.route("/home", methods=["GET"])
+@app.route('/', methods=['GET'])
+@app.route('/home', methods=['GET'])
 def home() -> 'html':
     return render_template('home.html', the_title='Home')
 
 
-@app.route('/register', methods=["GET", "POST"])
+@app.route('/register', methods=['GET', 'POST'])
 def new_user() -> 'html':
-    if request.method == "POST":
+    if request.method == 'POST':
         new_user = {'first_name': request.form['fname'],
                     'last_name':  request.form['lname'],
                     'age':        request.form['age'],
@@ -39,7 +39,7 @@ def new_user() -> 'html':
     return render_template('register.html', the_title='New Connection')
 
 
-@app.route("/login", methods=['GET', 'POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def login() -> 'html | Redirect':
     if request.method == 'POST':
         input_username = request.form['username']
@@ -49,10 +49,10 @@ def login() -> 'html | Redirect':
         if user:
             if check_hash(input_password, user[8]):
                 session['logged_in'] = True
-                msg = 'Login successful for ' + user[7]
+                msg = 'Login successful for {}'.format(user[7])
                 # flash(msg)
                 # return redirect(url_for('dashboard'))
-                return "<h2>{}</h2>".format(msg)
+                return '<h2>{}</h2>'.format(msg)
             else:
                 flash('Invalid password')
                 return redirect(url_for('login'))
@@ -61,10 +61,10 @@ def login() -> 'html | Redirect':
             flash('Invalid username')
             return redirect(url_for('login'))
             # return '<h2>Invalid username</h2>'
-    return render_template('login.html', the_title="Login")
+    return render_template('login.html', the_title='Login')
 
 
-@app.route('/logout', methods=["GET"])
+@app.route('/logout', methods=['GET'])
 def logout() -> 'html':
     return render_template('logout.html')
 
