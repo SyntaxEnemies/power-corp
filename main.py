4242df6ae495f07e08ee78a2c0c2f6fac4844a44
Rahul Sharma <rsharma842756@gmail.com>
2022-12-17 21:01:12 +0530

    feat(forms): Integrated validations in forms.
    Validation code is executed inside view functions before the form
    content is stored inside session cookie.
    Validation error messages are flashed directly to user after reseting
    the entire form.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import datetime
 from random import randint
 from re import fullmatch
 from typing import Literal
@@ -7,6 +8,9 @@ from flask import Flask, render_template, session, flash, redirect, url_for, req
 import crud
 from authutils import get_hash, check_hash, require_login
 from mailutils import create_mail_handler, obfuscate_mail_address, compose_html_mail
+from validations import (range_validator, type_validator, regex_validator,
+                         compare_validator, is_decimal_str, non_empty, is_email, is_correct_date,
+                         is_username, is_password, ValidationError)
 
 app = Flask(__name__)
 app.config.from_envvar('CONFIG_FILE')
@@ -33,26 +37,28 @@ def login() -> 'html | Redirect':
         return redirect(url_for('dashboard'))
 
     if request.method == 'POST':
-        input_username = request.form['username']
-        input_password = request.form['password']
+        try:
+            form_username = request.form['username'][:64]
+            form_password = request.form['password'][:64]
+        except KeyError as e:
+            print(str(e))
+            raise ValidationError('One or more fields were left blank.')
 
         # Return candidate user's record from the database (if any)
-        user = crud.get_user(input_username)
+        user = crud.get_user(form_username)
 
         if user:
             # print(user)
             # match given password's hash against one from the record
-            if check_hash(input_password, user['password']):
+            if check_hash(form_password, user['password']):
                 session['logged_in'] = True
+                # FIXME: flask flashes not visible in userpages
                 # msg = 'Login successful for {}'.format(user['username'])
                 # flash(msg)
-                return redirect(url_for('dashboard'))
                 # return '<h2>{}</h2> '.format(msg)
-            flash('Invalid password')
-            # return '<h2>Invalid password</h2>'
-        else:
-            flash('Invalid username')
-            # return '<h2>Invalid username</h2>'
+                return redirect(url_for('dashboard'))
+        # return '<h2>Incorrect username or password.</h2>'
+        flash('Incorrect username or password.')
     return render_template('login.html', the_title='Login')
 
 
@@ -60,25 +66,90 @@ def login() -> 'html | Redirect':
 def register() -> 'html | Redirect':
     """Render and accept account registration forms."""
     if request.method == 'POST':
-        # store registration form in cookie for lookup in later requests
-        # Basic registration particulars
-        session['reg'] = {}
-        session['reg']['basic'] = {'first_name': request.form['fname'],
-                                   'last_name':  request.form['lname'],
-                                   'age':        request.form['age'],
-                                   'address':    request.form['addr'],
-                                   'gender':     request.form['gender'],
-                                   'mobile_num': request.form['mobile_num'],
-                                   'email':      request.form['email'], }
-        # credit/debit card details
-        session['reg']['card'] = {'name':   request.form['card-name'],
-                                  'type':   request.form['card-type'],
-                                  'number': request.form['card-number'],
-                                  'cvv':    request.form['card-cvv'],
-                                  'expiry': request.form['card-expiry']}
+        try:
+            # Validating Basic User Information
+            if not non_empty(request.form['fname'][:128]):
+                raise ValidationError('Invalid first name.')
+            if not non_empty(request.form['lname'][:128]):
+                raise ValidationError('Invalid last name.')
+
+            if not range_validator(int(request.form['age']), 18, 150):
+                raise ValidationError('Your age is inappropriate for registration.')
+
+            genders = ('male', 'female', 'unsaid')
+            if not type_validator(request.form['gender'], genders):
+                raise ValidationError('Unknown gender type.')
+
+            if not is_email(request.form['email'][:256]):
+                raise ValidationError('Invalid email address provided.')
+
+            if not is_decimal_str(request.form['mobile_num'], 10):
+                raise ValidationError('Invalid phone number.')
+
+            if not non_empty(request.form['addr'][:256]):
+                raise ValidationError('Invalid address.')
+
+            # Basic registration particulars
+            basic_info = {'first_name': request.form['fname'][:128],
+                          'last_name':  request.form['lname'][:128],
+                          'age':        int(request.form['age']),
+                          'address':    request.form['addr'][:256],
+                          'gender':     request.form['gender'],
+                          'mobile_num': int(request.form['mobile_num']),
+                          'email':      request.form['email'][:256], }
+
+            # Validating given card details
+            if not regex_validator(request.form['card-name'][:32], r'[A-Z]+ [A-Z]+'):
+                raise ValidationError('Enter a valid card name.')
+
+            card_types = ('credit', 'debit')
+            if not type_validator(request.form['card-type'], card_types):
+                raise ValidationError('Unknown card type.')
+
+            if not is_decimal_str(request.form['card-number'], 16):
+                raise ValidationError('Invalid card number.')
+            if not is_decimal_str(request.form['card-cvv'], 3):
+                raise ValidationError('Invalid CVV.')
+
+            if not is_correct_date(request.form['card-expiry'], '%Y-%m'):
+                raise ValidationError('Card is already expired or invalid card expiry date.')
+
+            # Add day to card expiry date before storing in DB.
+            exp_date = datetime.datetime.strptime(request.form['card-expiry'], '%Y-%m')
+
+            # Calculate last day of month:
+            # 1. Move up one day to first day of next month.
+            # 2. Go back 1 day to last day of original month.
+
+            #    December is an exception.
+            if exp_date.month == 12:
+                exp_date = exp_date.replace(day=31)
+            else:
+                exp_date = (exp_date.replace(month=exp_date.month+1)
+                            - datetime.timedelta(days=1))
+
+            # credit/debit card details
+            card_info = {'name':   request.form['card-name'][:32],
+                         'type':   request.form['card-type'],
+                         'number': int(request.form['card-number']),
+                         'cvv':    int(request.form['card-cvv']),
+                         'expiry': exp_date}
+
+        except KeyError as e:
+            print(str(e))
+            raise ValidationError('One or more fields were left blank.')
+        except ValueError as e:
+            print(str(e))
+            raise ValidationError('Invalid value for a numeric field. ')
 
         # random six digit OTP to verify email
-        session['reg']['verify'] = {'otp': randint(100000, 999999)}
+        verify_info = {'otp': randint(100000, 999999)}
+
+        # store registration form in cookie for lookup in later requests
+        session['reg'] = {}
+        session['reg']['basic'] = basic_info
+        session['reg']['card'] = card_info
+        session['reg']['verify'] = verify_info
         print(session['reg'])
 
         return redirect(url_for('verify'))
@@ -90,6 +161,8 @@ def verify() -> 'Redirect':
     """Verify registration by sending OTP on email."""
     # Check if registration is initiated
     if 'reg' in session:
+        # FIXME: Email being resent for every wrong/invalid OTP.
+        # TODO: Separate email conversation for each OTP mail
         # construct html email message
         msg_subject = '[{}] - Confirm email with OTP'
         msg_subject = msg_subject.format(session['reg']['verify']['otp'])
@@ -99,7 +172,9 @@ def verify() -> 'Redirect':
                                     receiver=session['reg']['basic']['email'],
                                     subject=msg_subject,
                                     template='otp.html',
+                                    # Template arguments
                                     user=session['reg']['basic']['first_name'],
+                                    # TODO: Make more readable
                                     mail=obfuscate_mail_address(session['reg']['basic']['email']),
                                     otp=session['reg']['verify']['otp'])
 
@@ -108,19 +183,23 @@ def verify() -> 'Redirect':
                               message)
 
         if request.method == 'POST':
-            input_otp = ''
-            # Sort and concatenate the digits from the OTP input form
+            form_otp = ''
+            # Sort dict with otp digits to preserve order
+            print(request.form)
             for k, v in sorted(request.form.items()):
-                if fullmatch('d[0-9]+', k):
-                    input_otp += v
-            input_otp = int(input_otp)
+                # Validate OTP dict's keys and values.
+                if regex_validator(k, 'digit[1-6]') and is_decimal_str(v, 1):
+                    # Concatenate the digits from the OTP input form
+                    form_otp += v
+                else:
+                    raise ValidationError('Invalid OTP, OTP has been resent.')
 
-            if input_otp == session['reg']['verify']['otp']:
+            if int(form_otp) == session['reg']['verify']['otp']:
                 session['reg']['verify']['verified'] = True
                 session.modified = True
                 return redirect(url_for('set_credentials'))
 
-            flash('Incorrect OTP')
+            flash('Incorrect OTP, OTP has been resent.')
         return render_template('verify_email.html',
                                 the_title='Verify Email',
                                 mail=obfuscate_mail_address(session['reg']['basic']['email'])
@@ -135,16 +214,42 @@ def set_credentials() -> 'html | Redirect':
     if 'reg' in session and 'verify' in session['reg']:
         if 'verified' in session['reg']['verify']:
             if request.method == 'POST':
-                session['reg']['basic']['username'] = request.form['uname']
-                session['reg']['basic']['password'] = get_hash(request.form['passwd'])
+                try:
+                    form_username = request.form['username'][:64]
+                    if not is_username(form_username):
+                        errmsg = ('Username can only contain alphabets, '
+                                  'numerals, underscores, hyphens and periods.')
+                        raise ValidationError(errmsg)
+
+                    form_password = request.form['password'][:64]
+                    if not is_password(form_password):
+                        errmsg = ('Password must contain at least 8 characters '
+                                  'with at least a lowercase alphabet, an '
+                                  'uppercase alphabet, a numeral and one of '
+                                  '@$!%*?&.')
+                        raise ValidationError(errmsg)
+
+                    if not compare_validator(form_password,
+                                             request.form['confirm-password']):
+                        raise ValidationError('Passwords do not match.')
+
+                except KeyError as e:
+                    print(str(e))
+                    raise ValidationError('One or more fields were left blank.')
+
+                if compare_validator(form_password, form_username):
+                    raise ValidationError('Username cannot be the password.')
+
+                session['reg']['basic']['username'] = form_username
+                session['reg']['basic']['password'] = get_hash(form_password)
 
                 # Commit the registration into a database record
                 crud.add_user(session['reg']['basic'], session['reg']['card'])
 
                 # Confirmation message to user
-                msg = ('Dear {user}, your request for a new'
-                       'connection has been received. You can'
-                       'expect further developments within 24 hours')
+                msg = ('Dear {user}, your request for a new '
+                       'connection has been received. You can '
+                       'expect further developments within 24 hours.')
                 flash(msg.format(user=session['reg']['basic']['first_name']))
 
                 # Remove no longer needed data session cookie
@@ -172,11 +277,17 @@ def logout() -> 'html | Redirect':
     return redirect(url_for('home'))
 
 
-# @app.errorhandler(KeyError)
-# def invalid_form(e) -> 'Redirect':
-#     flash('Please fill all the required fields and try again')
-#     print(e)
-#     return redirect(request.url)
+@app.errorhandler(ValidationError)
+def invalid_form(err_msg) -> 'Redirect':
+    """Clear an invalid form and flash the validation issue.
+
+    Return a redirect to the current form which makes the browser clear
+    the form and display the indicated error in form validation to user
+    using a flash message.
+    """
+    print(str(err_msg))
+    flash(str(err_msg))
+    return redirect(request.url)
 
 
 @app.errorhandler(404)
