b7690c181208db3190af9e72ae09113230f8cb2b
Rahul Sharma <rsharma842756@gmail.com>
2022-12-17 20:40:51 +0530

    feat(db): Add 'sample_data' for database
    Switched to unsigned ints for ID fields.
    
    BREAKING CHANGE
    Changed the database name and username.
    Lowercased gender type.

--- database/db-schema.sql
+++ database/db-schema.sql
@@ -1,6 +1,6 @@
 -- MariaDB dump 10.19  Distrib 10.9.4-MariaDB, for Linux (x86_64)
 --
--- Host: localhost    Database: POWER_CORP
+-- Host: localhost    Database: power_corp
 -- ------------------------------------------------------
 -- Server version	10.9.4-MariaDB
 
@@ -23,7 +23,7 @@ DROP TABLE IF EXISTS `card_details`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `card_details` (
-  `uid` int(11) NOT NULL,
+  `uid` int(11) unsigned NOT NULL,
   `name` varchar(32) NOT NULL,
   `number` decimal(16,0) NOT NULL,
   `cvv` decimal(3,0) NOT NULL,
@@ -43,10 +43,10 @@ DROP TABLE IF EXISTS `consumption`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `consumption` (
-  `meter_id` int(11) NOT NULL AUTO_INCREMENT,
-  `id` int(11) NOT NULL,
-  `last_reading` int(11) NOT NULL DEFAULT 0,
-  `curr_reading` int(11) NOT NULL DEFAULT 0,
+  `meter_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `id` int(11) unsigned NOT NULL,
+  `last_reading` int(11) unsigned NOT NULL DEFAULT 0,
+  `curr_reading` int(11) unsigned NOT NULL DEFAULT 0,
   `due_date` date DEFAULT NULL,
   PRIMARY KEY (`meter_id`),
   KEY `fk_user_id` (`id`),
@@ -62,18 +62,18 @@ DROP TABLE IF EXISTS `emp_details`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `emp_details` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` smallint(11) unsigned NOT NULL AUTO_INCREMENT,
   `first_name` varchar(128) NOT NULL,
   `last_name` varchar(128) NOT NULL,
-  `age` int(11) NOT NULL CHECK (`age` >= 18),
+  `age` int(11) unsigned NOT NULL CHECK (`age` >= 18),
   `address` varchar(256) NOT NULL,
-  `gender` varchar(8) NOT NULL CHECK (`gender` in ('Male','Female','Unsaid')),
+  `gender` varchar(8) NOT NULL CHECK (`gender` in ('male','female','unsaid')),
   `join_date` timestamp NOT NULL DEFAULT current_timestamp(),
   `username` varchar(64) NOT NULL,
   `password` varchar(64) NOT NULL,
   `email` varchar(128) DEFAULT NULL CHECK (`email` like '%@%.%'),
   `mobile_num` decimal(10,0) NOT NULL,
-  `role` varchar(8) NOT NULL CHECK (`role` in ('Scout','Cashier','Admin')),
+  `role` varchar(8) NOT NULL CHECK (`role` in ('scout','cashier','admin')),
   PRIMARY KEY (`id`),
   UNIQUE KEY `username` (`username`),
   UNIQUE KEY `password` (`password`),
@@ -91,11 +91,11 @@ DROP TABLE IF EXISTS `payment_history`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `payment_history` (
-  `trans_id` int(11) NOT NULL AUTO_INCREMENT,
-  `uid` int(11) NOT NULL,
+  `trans_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `uid` int(11) unsigned NOT NULL,
   `amount` decimal(13,2) NOT NULL,
   `tstamp` timestamp NOT NULL DEFAULT current_timestamp(),
-  `mode` varchar(10) NOT NULL CHECK ('mode' in ('Credit Card','Debit Card')),
+  `mode` varchar(10) NOT NULL CHECK ('mode' in ('credit card','debit card')),
   `note` varchar(64) DEFAULT NULL,
   PRIMARY KEY (`trans_id`),
   KEY `uid` (`uid`),
@@ -111,12 +111,12 @@ DROP TABLE IF EXISTS `user_details`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `user_details` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `first_name` varchar(128) NOT NULL,
   `last_name` varchar(128) NOT NULL,
-  `age` int(11) NOT NULL CHECK (`age` >= 18),
+  `age` int(11) unsigned NOT NULL CHECK (`age` >= 18),
   `address` varchar(256) NOT NULL,
-  `gender` varchar(8) NOT NULL CHECK (`gender` in ('Male','Female','Unsaid')),
+  `gender` varchar(8) NOT NULL CHECK (`gender` in ('male','female','unsaid')),
   `issue_date` timestamp NOT NULL DEFAULT current_timestamp(),
   `username` varchar(64) NOT NULL,
   `password` varchar(64) NOT NULL,
@@ -140,4 +140,4 @@ CREATE TABLE `user_details` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-11-24 21:12:34
+-- Dump completed on 2022-12-15 16:52:26
