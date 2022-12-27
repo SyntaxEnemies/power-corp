93d9ec27cd1e15051833a98dc07e81ed6276adf0
Rahul Sharma <rsharma842756@gmail.com>
2022-12-27 15:08:28 +0530

    feat(scripts): Add shell script to add new user
    Bash script 'add-user.sh' uses curl to talk to the webserver and add a
    new user. This shell script is inside a new 'scripts' subdirectory.
    Existing shell script 'setup-venv.sh' is also moved there.

---
+++ scripts/add-user.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+# HTTP client config
+serv_port=5000
+cookie_file='cookie-jar'
+http_client="curl --silent --max-redirs 2 --cookie $cookie_file --cookie-jar $cookie_file --output /dev/null"
+
+if ! nc -z localhost "$serv_port"; then
+    printf "\033[31m%s\033[0m\n" "Start flask development server on port $serv_port first."
+    exit 1
+fi
+
+rand_str="$(tr -dc '[:upper:]' < /dev/urandom | head -c 6)"
+
+rand_num() {
+    if [[ -z $1 ]]; then
+        printf "Random number size not given.\nExiting ...\n"
+        exit 1
+    fi
+
+    tr -dc '[:digit:]' < /dev/urandom | head -c $1
+}
+
+# Basic details
+fname='test'
+lname="user$rand_str"
+age=18
+addr="test address $rand_str"
+gender='male'
+mobile_num="$(rand_num 10)"
+email="test$rand_str@example.com"
+
+# Credit/Debit card details
+cname="TEST USER$rand_str"
+ctype='credit'
+cnum="$(rand_num 16)"
+ccvv="$(rand_num 3)"
+cexpiry='2030-11'
+
+# Parse command-line args for username and password
+while getopts "u:p:" arg; do
+    case "$arg" in
+        u) uname="$OPTARG" ;;
+        p) pass="$OPTARG" ;;
+    esac
+done
+
+# if username and password not provided as args
+[[ -z "$uname" ]] && uname="test$rand_str"
+[[ -z "$pass" ]] && pass='AAaa..11'
+
+# User information request
+$http_client --data "fname=$fname&lname=$lname&age=$age&addr=$addr&gender=$gender&mobile-num=$mobile_num&email=$email&card-name=$cname&card-type=$ctype&card-number=$cnum&card-cvv=$ccvv&card-expiry=$cexpiry" "localhost:$serv_port/register"
+
+# Accept six digit OTP from user
+read -p "Enter OTP (from flask log): " -n 6 otp
+
+# Build verification request data form it.
+# First five OTP digits
+for ((i=0; i<5; ++i)); do
+    req_data="${req_data}digits[]=${otp:$i:1}&"
+done
+
+# Last OTP digit
+req_data="${req_data}digits[]=${otp:5:1}"
+
+# OTP verification request
+$http_client --data "$req_data" "localhost:$serv_port/verify"
+# Set login credentials request
+$http_client --data "username=$uname&password=$pass&confirm-password=$pass" "localhost:$serv_port/signup"
+
+rm "$cookie_file"
